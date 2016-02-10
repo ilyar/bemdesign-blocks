adfd41013c369ab151537765907d9efc66fdf18b
Ilya Rogov <ilyar.software@gmail.com>
2016-02-10 08:23:57 +0200

    config bemdesign-components as library

--- .enb/make.js
+++ .enb/make.js
@@ -26,8 +26,8 @@ var techs = {
         { path: 'libs/bem-core/desktop.blocks', check: false },
         { path: 'libs/bem-components/common.blocks', check: false },
         { path: 'libs/bem-components/desktop.blocks', check: false },
-        { path: 'libs/bem-components/design/common.blocks', check: false },
-        { path: 'libs/bem-components/design/desktop.blocks', check: false },
+        { path: 'libs/bemdesign-components/common.blocks', check: false },
+        { path: 'libs/bemdesign-components/desktop.blocks', check: false },
         { path: 'libs/bem-grid/common.blocks', check: false },
         'common.blocks',
         'desktop.blocks'
